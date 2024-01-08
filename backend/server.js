5971d5a577f734d1d31b021ddcf11119a727e805
Jason Simms <simmsthecoder@gmail.com>
2024-01-08 15:49:40 +0100

    express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 const { Firestore } = require('@google-cloud/firestore');
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
 
 // Replace 'your-project-id' with your actual Google Cloud project ID
 const projectId = 'todo-18f70';
@@ -6,7 +13,7 @@ const projectId = 'todo-18f70';
 // Initialize Firestore with the project ID
 const firestore = new Firestore({
   projectId: projectId,
-  keyFilename: '../firebaseAccountKeys.json', // Specify the path to your service account key file
+  keyFilename: './firebaseAccountKeys.json', // Specify the path to your service account key file
 });
 
 // Now you can use the 'firestore' object to interact with Firestore
@@ -14,12 +21,31 @@ const firestore = new Firestore({
 const collectionRef = firestore.collection('users');
 
 
-async function foo () {
+async function foo() {
 
-    const snapshot = await collectionRef.get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    });
+  const snapshot = await collectionRef.get();
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+  });
+  return snapshot;
 }
 
-foo();
+// foo();
+
+
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.get('/foo', (req, res) => {
+  foo().then(results => {
+    console.log('results here =>', typeof results, results)
+    // results = json.stringify(results);
+    res.send(results);
+  })
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
